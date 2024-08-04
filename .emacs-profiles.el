03b9e14d350e512d61011b83f195661d567a6e02
EmmanueleSalvati <emmanuele.salvati@gmail.com>
2024-08-04 12:15:21 -0400

    Adding personal emacs configuration

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,3 +1,4 @@
 (("default" . ((user-emacs-directory . "~/.emacs.default") (server-name . "default")))
 ("doom" . ((user-emacs-directory . "~/Code/doom-emacs") (server-name . "doom")))
-("syscraft" . ((user-emacs-directory . "~/.emacs.syscraft") (server-name . "syscraft"))))
+("syscraft" . ((user-emacs-directory . "~/.emacs.syscraft") (server-name . "syscraft")))
+("personal" . ((user-emacs-directory . "~/.emacs.personal") (server-name . "personal"))))
