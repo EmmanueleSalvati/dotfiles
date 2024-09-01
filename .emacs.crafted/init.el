2162d3113fbdc279f652c90956157b40e7558ad0
EmmanueleSalvati <emmanuele.salvati@gmail.com>
2024-09-01 15:42:48 -0400

    crafted setup

--- .emacs.crafted/init.el
+++ .emacs.crafted/init.el
@@ -4,3 +4,24 @@
   (load custom-file nil :nomessage))
 
 (load "~/Code/crafted-emacs/modules/crafted-init-config")
+
+(require 'crafted-startup-config)
+(require 'crafted-defaults-config)
+(require 'crafted-completion-packages)
+(require 'crafted-ide-packages)
+(require 'crafted-org-packages)
+(add-to-list 'package-selected-packages 'org-roam)
+
+(package-install-selected-packages :noconfirm)
+
+(require 'crafted-ide-config)
+
+(add-hook 'prog-mode-hook #'aggressive-indent-mode)
+(crafted-ide-eglot-auto-ensure-all)
+(crafted-ide-configure-tree-sitter '(css html python))
+
+(require 'crafted-org-config)
+
+;; add crafted-completion package definitions to selected packages list
+
+(require 'crafted-completion-config)
